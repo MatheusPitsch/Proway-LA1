4c60c2202e928d14eae8dbd178976167619def63
MatheusPitsch <matheuspitschh@gmail.com>
2022-04-08 21:30:21 -0300

    Aula 06

--- Aula 06/Aula 06 - Matrizes/Exercicos06/Ex9.py
+++ Aula 06/Aula 06 - Matrizes/Exercicos06/Ex9.py
@@ -4,54 +4,50 @@ matriz = [
     ['*', '*', '*'],
     ['*', '*', '*']
 ]
+d = True
 
-while True:
+while d == True:
 
     for i, d in enumerate(matriz):#VERIFICAR DE PARADA
-        for q, linha in enumerate(matriz): #PARA COLOCAR A VARIAVEL LINHA AQUI
-            if q == '*':
-                print('Tudo certo')
-            else:    
-                break
-
-    
-    matriz = [ [0 for i in range(3)] for j in range(3)]
-        
+        if matriz != ['*']:
+            None
+        else:    
+            d == False
 
 
     A = int(input('Digite uma posição X: '))
     A_1 = int(input('Digite uma posição Y: '))
    
-    
+    for i, d in enumerate(matriz):#VERIFICAR DE PARADA
+        if matriz != ['*']:
+            None
+        else:    
+            d == False
+
     if matriz[A][A_1] == '*':
         matriz[A][A_1] = 'X'
     else:
         print('Essa posição ja esta ocupada!')
         continue
     
-    for i, d in enumerate(matriz):#VERIFICAR DE PARADA
-        for q, linha in enumerate(matriz): #PARA COLOCAR A VARIAVEL LINHA AQUI
-            if q == '*':
-                print('Tudo certo')
-            else:    
-                break
 
-
+    
     B = int(input('Digite uma posição I: '))
     B_1 = int(input('Digite uma posição M: '))
     
+    for i, d in enumerate(matriz):#VERIFICAR DE PARADA
+        if matriz != ['*']:
+            None
+        else:    
+            d == False
+
     if matriz[B][B_1] == '*':
         matriz[B][B_1] = '0'
     else:
         print('Essa posição ja esta ocupada!')
         continue
 
-    for i, d in enumerate(matriz):#VERIFICAR DE PARADA
-        for q, linha in enumerate(matriz): #PARA COLOCAR A VARIAVEL LINHA AQUI
-            if q == '*':
-                print('Tudo certo')
-            else:    
-                break
+    
     
     
 
