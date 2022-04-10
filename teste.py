24d601cc64eee25d3ae2b09a94029660bfb1d9a1
MatheusPitsch <matheuspitschh@gmail.com>
2022-04-10 13:35:14 -0300

    Teste desafio

--- teste.py
+++ teste.py
@@ -1,20 +1,4 @@
-matriz_alunos = [[71719,'matheus']]
-def verificacao_matricula(matricula) -> bool:
-    for i  in matriz_alunos:
-        if i[0] == matricula:
-            return True
-    return False
-def editar_informacao_aluno() -> str:
-    '''Editar o nome do aluno.'''
-    matricula = int(input('Digite a matricula do aluno que deseja alterar o nome: '))
-    
-    if verificacao_matricula(matricula) == True:
-        nome_novo = str(input('Digite o novo nome do aluno: '))
-        for i in matriz_alunos:
-            if i[0] == matricula:
-                i[1] = nome_novo
-    else:
-        input('Aluno não encontrado')
+matriz_alunos = []
 def cadastro_aluno():
     '''Retorna a identificação do aluno.'''
     aluno = []
@@ -26,5 +10,27 @@ def cadastro_aluno():
         aluno.append([nome,matricula,'--','--','--'])
         matriz_alunos.append(aluno)
 
-editar_informacao_aluno()
+def verificacao_matricula(matricula) -> bool:
+    for i  in matriz_alunos:
+        if i[0] == matricula:
+            return True
+    return False
+
+def cadastrar_notas_aluno():
+    '''Cadastra as 3 notas do aluno.'''
+    matricula = int(input('Digite a matricula do aluno que deseja insirir as notas.'))
+    if verificacao_matricula == True:
+        for i in range(3):
+            nota = float(input(f'Digite a nota {i} do aluno: '))
+            for i in matriz_alunos:
+                if i[2] == '--':
+                    i[2] == nota
+                elif i[3] == '--':
+                    i[3] == nota
+                elif i[4] == '--':
+                    i[4] == nota
+
+cadastro_aluno()
+print(matriz_alunos)
+cadastrar_notas_aluno()
 print(matriz_alunos)
