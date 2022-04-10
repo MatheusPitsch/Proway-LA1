76dc46d6b0b1d3200b4ceadbd8cb5c01a8022d52
MatheusPitsch <matheuspitschh@gmail.com>
2022-04-10 13:35:03 -0300

    Desafio

--- Ex9.py
+++ Ex9.py
@@ -15,7 +15,6 @@ def mostrar_menu():
     0) Sair
     ''')
 
-
 def cadastro_aluno():
     '''Retorna a identificação do aluno.'''
     aluno = []
@@ -47,6 +46,18 @@ def editar_informacao_aluno():
 
 def cadastrar_notas_aluno():
     '''Cadastra as 3 notas do aluno.'''
+    matricula = int(input('Digite a matricula do aluno que deseja insirir as notas.'))
+    if verificacao_matricula == True:
+        for i in range(3):
+            nota = float(input(f'Digite a nota {i} do aluno:'))
+            for i in matriz_alunos:
+                if i[2] == '--':
+                    i[2] == nota
+                elif i[3] == '--':
+                    i[3] == nota
+                elif i[4] == '--':
+                    i[4] == nota
+
 def listar_alunos():
     '''Lista todos os alunos que foram cadastrados.'''
 
